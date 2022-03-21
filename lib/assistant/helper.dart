865f89b49f924c3c46644577a761dfae6d31cc78
Md. Shuhayel Alom Shanto <73125478+AlomShanto@users.noreply.github.com>
2022-03-21 11:50:37 +0600

    Delete lib directory

--- lib/assistant/helper.dart
+++
@@ -1,6 +0,0 @@
-class Helper{
-  static String baseUrl = "https://codeforces.com/api/";
-  static String userInfo = baseUrl+"user.info?handles=";
-  static String userContestRecord = baseUrl+"user.rating?handle=";
-  static String userSubmissions= baseUrl+"user.status?handle=";
-}
